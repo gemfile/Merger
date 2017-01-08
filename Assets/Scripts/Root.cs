0f02a620636bf665e5f81f2f310b87c9e9339fe6
박기용 <usebydriving@gmail.com>
2017-01-08 18:17:53 +0900

    Make monster’s animation

---
+++ Assets/Scripts/Root.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts.Game;
+using System.Linq;
+
+public class Root : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//        new Coin();
+//        new Potion();
+//        new Weapon();
+//        new Magic();
+//        new Monster();
+
+        var potionValues = Enumerable.Range(2, 9);
+        var decks = new List<>();
+
+        foreach (int value in potionValues)
+        {
+            new Coin(value);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
