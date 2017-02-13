061b82b163571d89af500394dc37805798e24cf1
박기용 <gemfile0@gmail.com>
2017-02-13 20:41:19 +0900

    Add a util which shows all of bounds of children.

---
+++ Assets/Scripts/Util/Editor/ShowBoundsEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace com.Gemfile.Merger
+{
+	[CustomEditor(typeof(ShowBounds))]
+	internal class ShowBoundsEditor : Editor
+	{
+		void OnSceneGUI( )
+		{
+			ShowBounds t = target as ShowBounds;
+
+			if(t == null || t.edges == null)
+				return;
+
+			Handles.color = Color.magenta;
+
+			Vector3 endPoint = t.edges[0];
+			t.edges.ForEach(edge => {
+				Handles.DrawLine(endPoint, edge);
+				endPoint = edge;
+			});
+		}
+	}
+}
